e65304722389f5badfe0cfd56e8d5ab5b8218668
David5183 <jacobsdavid.dr@gmail.com>
2024-12-07 18:57:17 +0100

    Update home route

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const dotenv = require("dotenv");
+const express = require("express");
 
 dotenv.config({ path: "./config.env" });
+
+// Import the app instance
 const app = require("./app");
 
+// Add a root route to display the welcome message
+app.get("/", (req, res) => {
+  res.send("Welcome to NotifyMe API built with Node and Express!");
+});
+
+// Start the server
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
